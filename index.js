f088b32238ecdbc623a6c8511b5fc121ba50dc6f
vkasyan76 <vkasyan@gmx.de>
2023-02-02 12:45:47 +0100

    openaiController and index.html for post requests checks

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const { generateImage } = require('../controllers/openaiController')
 const router = express.Router()
 
-router.post('/generateimage', (req, res) => {
-    res.status(200).json({
-        success: true,
-    })
-})
+router.post('/generateimage', generateImage)
+
+// router.post('/generateimage', (req, res) => {
+//     console.log(req.body)
+//     res.status(200).json({
+//         success: true,
+//     })
+// })
 
 module.exports = router
